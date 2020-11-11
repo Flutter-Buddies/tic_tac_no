10a217885ab267c5929440043fb9a9808102b6cd
Sandro Lovnički <sandro@scipack.xyz>
2020-11-11 22:17:51 +0100

    {game_screen}: [add] ai thinking loader

--- lib/game/ui/game_screen.dart
+++ lib/game/ui/game_screen.dart
@@ -33,37 +33,66 @@ class GameScreenState extends State<GameScreen> {
       },
       child: Scaffold(
         backgroundColor: Colors.green,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            BlocBuilder<GameBloc, GameState>(builder: (context, state) {
-              if (state is GameOver) {
-                if (state.winner == null) {
-                  return Text('TIE!');
-                } else {
-                  return Text('Winner is ${state.winner.name}!');
-                }
-              }
-              return SizedBox.shrink();
-            }),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('TURN: ${this._currentPlayer.name}'),
-                Container(
-                  width: 20,
-                  height: 20,
-                  color: this._currentPlayer.color,
-                )
-              ],
-            ),
-            SizedBox(height: 16),
-            GridWidget(
-              grid: this._grid,
-            )
-          ],
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 0,
+                child:
+                    BlocBuilder<GameBloc, GameState>(builder: (context, state) {
+                  if (state is GameOver) {
+                    if (state.winner == null) {
+                      return Text('TIE!');
+                    } else {
+                      return Text('Winner is ${state.winner.name}!');
+                    }
+                  }
+                  return SizedBox.shrink();
+                }),
+              ),
+              Positioned(
+                top: 0,
+                child:
+                    BlocBuilder<GameBloc, GameState>(builder: (context, state) {
+                  if (state is AIThinking) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('AI is thinking...'),
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: CircularProgressIndicator(),
+                        ),
+                      ],
+                    );
+                  }
+                  return SizedBox.shrink();
+                }),
+              ),
+              Positioned(
+                top: 32,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('TURN: ${this._currentPlayer.name}'),
+                    Container(
+                      width: 20,
+                      height: 20,
+                      color: this._currentPlayer.color,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 64.0),
+                child: GridWidget(
+                  grid: this._grid,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
