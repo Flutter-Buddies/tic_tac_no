54d6a5518b772ae16f208a47dfdc42758e9d3296
Sandro Lovnički <sandro@scipack.xyz>
2020-11-12 21:38:58 +0100

    {position}: [extend] Equatable

--- lib/game/data/models/position.dart
+++ lib/game/data/models/position.dart
@@ -1,6 +1,11 @@
-class Position {
-  int x;
-  int y;
+import 'package:equatable/equatable.dart';
+
+class Position extends Equatable {
+  final int x;
+  final int y;
 
   Position(this.x, this.y);
+
+  @override
+  List<Object> get props => [x, y];
 }
