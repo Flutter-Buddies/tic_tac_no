9cbd961f725b548f2c113d6bdc018a8c774eafaa
doug <doug.todd@hey.com>
2020-11-26 20:57:57 +0000

    first version of home screen

---
+++ lib/menu/ui/piece_shapes.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class CrossPainter extends CustomPainter {
+  CrossPainter({this.drawingColor});
+  final Color drawingColor;
+  @override
+  void paint(Canvas canvas, Size size) {
+    // paint is the brush
+    var paint = Paint()
+      ..color = drawingColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 14;
+
+    // path is the drawing
+    var path = Path()
+      ..lineTo(size.width, size.height)
+      ..moveTo(0, size.height)
+      ..lineTo(size.width, 0);
+
+    // Todo: get shadow working
+    canvas.drawShadow(path, Colors.black, 5.0, false);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class TrianglePainter extends CustomPainter {
+  TrianglePainter({this.drawingColor});
+  final Color drawingColor;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = drawingColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 14;
+
+    var path = Path()
+      ..moveTo(size.width / 2, 0)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class CirclePainter extends CustomPainter {
+  CirclePainter({this.drawingColor});
+  final Color drawingColor;
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = drawingColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 14;
+
+    var path1 = Path()
+      ..addOval(Rect.fromCircle(
+          center: Offset(size.width / 2, size.height / 2), radius: 50));
+
+    canvas.drawPath(path1, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class SquirclePainter extends CustomPainter {
+  SquirclePainter({this.drawingColor});
+  final Color drawingColor;
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = drawingColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 14;
+
+    var path1 = Path()
+      ..addRRect(
+        RRect.fromLTRBR(
+          0,
+          0,
+          size.width,
+          size.height,
+          Radius.circular(35),
+        ),
+      );
+    canvas.drawPath(path1, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
---
+++ lib/menu/ui/title.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TitleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'TIC',
+          style: GoogleFonts.firaCode(textStyle: Theme.of(context).textTheme.headline1),
+        ),
+        Text(
+          'TAC',
+          style: GoogleFonts.firaCode(textStyle: Theme.of(context).textTheme.headline1),
+        ),
+        Text(
+          'NO',
+          style: GoogleFonts.firaCode(textStyle: Theme.of(context).textTheme.headline1),
+        ),
+      ],
+    );
+  }
+}
