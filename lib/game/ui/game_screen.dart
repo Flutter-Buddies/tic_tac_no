3575487fcf007a0a1072ede65c234567b8157cf5
doug <doug.todd@hey.com>
2020-12-06 22:06:38 +0000

    updated styling of board

--- lib/game/ui/game_screen.dart
+++ lib/game/ui/game_screen.dart
@@ -32,66 +32,73 @@ class GameScreenState extends State<GameScreen> {
         }
       },
       child: Scaffold(
-        backgroundColor: Colors.green,
-        body: Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                top: 0,
-                child:
-                    BlocBuilder<GameBloc, GameState>(builder: (context, state) {
-                  if (state is GameOver) {
-                    if (state.winner == null) {
-                      return Text('TIE!');
-                    } else {
-                      return Text('Winner is ${state.winner.name}!');
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xff1E3C72), Color(0xff2A5298)],
+          )),
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  top: 0,
+                  child: BlocBuilder<GameBloc, GameState>(
+                      builder: (context, state) {
+                    if (state is GameOver) {
+                      if (state.winner == null) {
+                        return Text('TIE!');
+                      } else {
+                        return Text('Winner is ${state.winner.name}!');
+                      }
                     }
-                  }
-                  return SizedBox.shrink();
-                }),
-              ),
-              Positioned(
-                top: 0,
-                child:
-                    BlocBuilder<GameBloc, GameState>(builder: (context, state) {
-                  if (state is AIThinking) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('AI is thinking...'),
-                        SizedBox(
-                          width: 20,
-                          height: 20,
-                          child: CircularProgressIndicator(),
-                        ),
-                      ],
-                    );
-                  }
-                  return SizedBox.shrink();
-                }),
-              ),
-              Positioned(
-                top: 32,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('TURN: ${this._currentPlayer.name}'),
-                    Container(
-                      width: 20,
-                      height: 20,
-                      color: this._currentPlayer.color,
-                    )
-                  ],
+                    return SizedBox.shrink();
+                  }),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 64.0),
-                child: GridWidget(
-                  grid: this._grid,
+                Positioned(
+                  top: 0,
+                  child: BlocBuilder<GameBloc, GameState>(
+                      builder: (context, state) {
+                    if (state is AIThinking) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('AI is thinking...'),
+                          SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: CircularProgressIndicator(),
+                          ),
+                        ],
+                      );
+                    }
+                    return SizedBox.shrink();
+                  }),
                 ),
-              )
-            ],
+                Positioned(
+                  top: 32,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('TURN: ${this._currentPlayer.name}'),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        color: this._currentPlayer.color,
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 64.0),
+                  child: GridWidget(
+                    grid: this._grid,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
