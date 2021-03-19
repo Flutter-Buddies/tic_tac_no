16c555eff3d55f119347c5614f1430405e9b2974
doug-todd <doug.todd@hey.com>
2021-03-19 17:07:09 +0000

    [Audio stopped working] Updated audio files
    Audio files have been converted to mp3 and normalised. However, audio is now not playing.

--- lib/utils/audio.dart
+++ lib/utils/audio.dart
@@ -21,42 +21,44 @@ class GameAudio extends BaseAudio {
   void preloadSounds() {
     player.loadAll([
       'piece_placement_1.mp3',
-      'success_1.wav',
-      'player_win_1.wav',
-      'game_over_1.wav',
+      'success_1.mp3',
+      'player_win_1.mp3',
+      'game_over_1.mp3',
     ]);
   }
 
   void playSound(GameSounds soundEvent) async {
     switch (soundEvent) {
       case GameSounds.PlacingPiece:
-        await player.play('piece_placement_1.mp3',
-            mode: PlayerMode.LOW_LATENCY,
-            volume: 1.0 * (super.isMuted ? 0.0 : 1.0));
+        await player.play(
+          'piece_placement_1.mp3',
+          mode: PlayerMode.LOW_LATENCY,
+          volume: 1 * (super.isMuted ? 0.0 : 1.0),
+        );
         break;
       case GameSounds.InnerGridWin:
         await player.play(
-          'success_1.wav',
+          'success_1.mp3',
           mode: PlayerMode.LOW_LATENCY,
-          volume: 0.2 * (super.isMuted ? 0.0 : 1.0),
+          volume: 1 * (super.isMuted ? 0.0 : 1.0),
         );
         break;
       case GameSounds.GameWon:
         // Need to wait for inner grid win to finish playing
         await Future.delayed(const Duration(milliseconds: 500));
         await player.play(
-          'player_win_1.wav',
+          'player_win_1.mp3',
           mode: PlayerMode.LOW_LATENCY,
-          volume: 0.2 * (super.isMuted ? 0.0 : 1.0),
+          volume: 1 * (super.isMuted ? 0.0 : 1.0),
         );
         break;
       case GameSounds.GameLost:
         // Ned to wait for innter grid win the finish playing
         await Future.delayed(const Duration(milliseconds: 500));
         await player.play(
-          'game_over_1.wav',
+          'game_over_1.mp3',
           mode: PlayerMode.LOW_LATENCY,
-          volume: 0.2 * (super.isMuted ? 0.0 : 1.0),
+          volume: 1 * (super.isMuted ? 0.0 : 1.0),
         );
         break;
     }
@@ -67,7 +69,7 @@ class UIAudio extends BaseAudio {
   @override
   void preloadSounds() {
     player.loadAll([
-      'button_click_1.wav',
+      'button_click_1.mp3',
     ]);
   }
 
@@ -75,9 +77,9 @@ class UIAudio extends BaseAudio {
     switch (uiSound) {
       case UISounds.ButtonClick:
         await player.play(
-          'button_click_1.wav',
+          'button_click_1.mp3',
           mode: PlayerMode.LOW_LATENCY,
-          volume: 0.5 * (super.isMuted ? 0.0 : 1.0),
+          volume: 1 * (super.isMuted ? 0.0 : 1.0),
         );
         break;
     }
