6ccfc6590ba3c69a3cbbb1504db4fa9c9ba799d5
Matej Zidek <zidek.matt@gmail.com>
2021-04-21 17:48:22 +0200

    Migrate game bloc

--- lib/game/bloc/game_state.dart
+++ lib/game/bloc/game_state.dart
@@ -11,23 +11,26 @@ class JudgeThinking extends GameState {}
 
 class Ready extends GameState {
   Ready({
-    @required this.grid,
-    @required this.players,
-    @required this.currentPlayer,
-    @required this.score,
-    @required this.winner,
+    required this.grid,
+    this.players,
+    this.currentPlayer,
+    this.score,
+    this.winner,
   });
 
   final Grid grid;
-  final List<Player> players;
-  final Player currentPlayer;
-  final Map<int, int> score;
-  final Player winner;
+  final List<Player>? players;
+  final Player? currentPlayer;
+  final Map<int, int>? score;
+  final Player? winner;
 }
 
 class GameOver extends GameState {
-  GameOver({this.winner, this.winningPositions});
+  GameOver({
+    this.winner,
+    this.winningPositions,
+  });
 
-  final Player winner;
-  final WinningPositions winningPositions;
+  final Player? winner;
+  final WinningPositions? winningPositions;
 }
